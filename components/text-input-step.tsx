76c866ee9da8c375c22882636621f7a52c7ef0b0
vetorL <145482274+vetorL@users.noreply.github.com>
2025-04-18 14:30:07 -0300

    Implement TextInputStep

--- components/text-input-step.tsx
+++ components/text-input-step.tsx
@@ -1,3 +1,39 @@
-export default function TextInputStep() {
-  return <></>;
+import { Voice } from "@/lib/types";
+import TextField from "@mui/material/TextField";
+
+interface TextInputStepProps {
+  text: string;
+  setText: (text: string) => void;
+  selectedVoice: Voice | null;
+}
+
+export default function TextInputStep({
+  text,
+  setText,
+  selectedVoice,
+}: TextInputStepProps) {
+  return (
+    <>
+      <ul className="list-inside text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
+        <li className="mb-2 tracking-[-.01em]">
+          - Write the text to be turned into audio
+        </li>
+        <li className="mb-2 tracking-[-.01em] text-gray-500">
+          Note: you chose <i>{selectedVoice?.name}</i> as the voice, make sure
+          to write in <i>{selectedVoice?.language}</i> for the best results.
+        </li>
+      </ul>
+
+      <TextField
+        id="outlined-basic"
+        label="Text Input"
+        variant="outlined"
+        fullWidth
+        multiline
+        rows={5}
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
+    </>
+  );
 }
