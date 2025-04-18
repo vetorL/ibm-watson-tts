261086f1375355e51820a42197b9f9ed35d6cc81
vetorL <145482274+vetorL@users.noreply.github.com>
2025-04-18 14:41:47 -0300

    Implement AudioOutputStep

--- components/audio-output-step.tsx
+++ components/audio-output-step.tsx
@@ -1,3 +1,28 @@
+import { Card, CardContent, Typography } from "@mui/material";
+
 export default function AudioOutputStep() {
-  return <></>;
+  return (
+    <>
+      <ul className="list-inside text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
+        <li className="mb-2 tracking-[-.01em]">
+          - Audio generated succesfully!
+        </li>
+      </ul>
+      <AudioPlayer src="https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3" />
+    </>
+  );
+}
+
+function AudioPlayer({ src, title }: { src: string; title?: string }) {
+  return (
+    <Card variant="outlined">
+      <CardContent>
+        {title && <Typography variant="h6">{title}</Typography>}
+        <audio controls style={{ width: "100%" }}>
+          <source src={src} type="audio/mpeg" />
+          Your browser does not support the audio element.
+        </audio>
+      </CardContent>
+    </Card>
+  );
 }
