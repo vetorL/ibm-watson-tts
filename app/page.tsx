a903128f5046bfbf3b512f8d1868c5351ca341f8
vetorL <145482274+vetorL@users.noreply.github.com>
2025-04-21 14:25:09 -0300

    Add proper link to GitHub repository

--- app/page.tsx
+++ app/page.tsx
@@ -43,12 +43,12 @@ export default function Home() {
         </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          href="https://github.com/vetorL/ibm-watson-tts"
           target="_blank"
           rel="noopener noreferrer"
         >
           <GitHubIcon aria-hidden width={16} height={16} />
-          GitHub Repository
+          GitHub
         </a>
       </footer>
     </div>
