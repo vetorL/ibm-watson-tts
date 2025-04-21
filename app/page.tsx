8940ac69f75890cc75b75b7b587af1888726c10f
vetorL <145482274+vetorL@users.noreply.github.com>
2025-04-21 14:43:51 -0300

    Remove unnecessary links

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import TextToSpeechStepper from "@/components/text-to-speech-stepper";
 import GitHubIcon from "@mui/icons-material/GitHub";
-import Image from "next/image";
 
 export default function Home() {
   return (
@@ -11,36 +10,6 @@ export default function Home() {
         <TextToSpeechStepper />
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Docs
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Tutorial
-        </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
           href="https://github.com/vetorL/ibm-watson-tts"
